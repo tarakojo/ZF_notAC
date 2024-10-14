1f1eb41d0ab0c3cc5a40200ae368148c740b7fea
tarakojo <tarakojo1019@gmail.com>
2024-10-14 09:24:14 +0900

    updated

--- code/Test.thy
+++ code/Test.thy
@@ -2,6 +2,29 @@ theory Test
   imports ZF_notAC 
 begin 
 
+lemma test_lemma : 
+  fixes A B 
+  assumes "A = { 0 }" "B = { 1 }" 
+  shows "A \<union> B \<subseteq> { 0, 1, 2 }" 
+proof(rule subsetI) 
+  fix x 
+  assume x_in_A_union_B : "x \<in> A \<union> B" 
+  show "x \<in> { 0, 1, 2 }" 
+  proof (cases "x \<in> A")
+    case True
+    then have "x = 0" using assms by simp
+    then show "x \<in> { 0, 1, 2 }" by simp
+  next
+    case False
+    then have "x \<in> B" using x_in_A_union_B by simp
+    then show "x \<in> { 0, 1, 2 }" using assms by simp 
+  qed
+qed
 
-
-end 
+lemma test_lemma2 : 
+  fixes A B 
+  assumes "A = { 0 }" "B = { 1 }" 
+  shows "A \<union> B \<subseteq> { 0, 1, 2 }" 
+  using assms 
+  by auto
+end
